8a240a962fbc07cfebeed925d19e4fd2bf2b8be9
sengkheng <thanapol.suwannamek@hotmail.com>
2025-10-02 13:23:06 +0700

    Rename tkinter main window variable from root to main.

--- shutdowntimer.py
+++ shutdowntimer.py
@@ -1,22 +1,22 @@
 import tkinter
 
-root = tkinter.Tk()
+main = tkinter.Tk()
 
 #Window configuration
 win_width = 400
 win_height = 400
 
 #Screen resolution
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+screen_width = main.winfo_screenwidth()
+screen_height = main.winfo_screenheight()
 
 #Center position of window
 x = (screen_width // 2) - (win_width // 2)
 y = (screen_height // 2) - (win_height // 2)
 
 
-root.title("Shutdown Timer")
+main.title("Shutdown Timer")
 print(screen_width, " : ", screen_height)
 
-root.geometry(f"{win_width}x{win_height}+{x}+{y}")
-root.mainloop()
+main.geometry(f"{win_width}x{win_height}+{x}+{y}")
+main.mainloop()
