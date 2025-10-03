b2f2e3380929f139a9af58bbd3e890b6845f6d8f
abdulthemagician <thanapol.suwannamek@hotmail.com>
2025-10-03 15:02:08 +0700

    Add setShutdownTimer and bind to button

--- shutdowntimer.py
+++ shutdowntimer.py
@@ -20,26 +20,62 @@ screen_height = main.winfo_screenheight()
 x = (screen_width // 2) - (win_width // 2)
 y = (screen_height // 2) - (win_height // 2)
 
+def setShutdownTimer(hour: int, minute: int):
+    second = (hour * 3600) + (minute * 60)
+    if second <= 0:
+        confirm = messagebox.askyesno("0 minutes, 0 second", "คุณต้องการปิดเครื่องทันทีหรือไม่ ?")
+        if confirm:
+            rc = os.system(f"shutdown -s -t 5")
+            if rc != 0:
+                os.system("shutdown -a")
+                os.system(f"shutdown -s -t 5")
+            messagebox.showinfo("ตั้งเวลาปิดเครื่อง", f"เครื่องกำลังจะปิดในอีก 5 วินาที")
+            return
+        else:
+            return
+        
+    if second > 0:
+        rc = os.system(f"shutdown -s -t {second}")
+        if rc != 0:
+            answer = messagebox.askyesno("การตั้งเวลาปิดเครื่องทับซ้อน", "ตรวจพบว่ามีการตั้งเวลาปิดเครื่องอยู่แล้ว\nต้องการยกเลิกของเก่าและตั้งค่าใหม่หรือไม่?")
+            if not answer:
+                return
+            else:
+                os.system("shutdown -a")
+                os.system(f"shutdown -s -t {second}")
+                
+        if hour <= 0 and minute > 0:
+            messagebox.showinfo("ตั้งเวลาปิดเครื่อง", f"ตั้งเวลาปิดเครื่องใน {second / 60:.0f} นาทีแล้ว")
+        else:
+            messagebox.showinfo("ตั้งเวลาปิดเครื่อง", f"ตั้งเวลาปิดเครื่องใน {hour} ชั่วโมง {minute} นาทีแล้ว")
+        
+def abortShutdownTimer():
+    os.system("shutdown -a")
+    messagebox.showinfo("ยกเลิก", "ยกเลิกการปิดเครื่องเสร็จสิ้น")
+
+def showMessage():
+    hour = int(hours.get())
+    minute = int(minutes.get())
+    print(hour, minute)
+
 #Text Box
 hours = tk.StringVar(value=0)
 minutes = tk.IntVar(value=0)
 
-hourComboBox = ttk.Combobox(textvariable=hours)
-hourComboBox["values"] = [1, 2, 3, 4, 5]
-hourComboBox.place(x=10, y=win_height - 120, width=40)
+hourBox = ttk.Spinbox(textvariable=hours, increment=1, from_=0, to=10)
+hourBox.place(x=20, y=win_height - 120, width=40)
 
-minuteComboBox = ttk.Combobox(textvariable=minutes)
-minuteComboBox["values"] = [15, 30, 45]
-minuteComboBox.place(x=100, y=win_height - 120, width=40)
+minuteBox = ttk.Spinbox(textvariable=minutes, increment=10, from_=1, to=60)
+minuteBox.place(x=110, y=win_height - 120, width=40)
 
 #Label
 shutdownTimerLabel = tk.Label(main, text="Shutdown Timer", bg="#a6edff", fg="Black", font=10).place(x=40, y=20)
-hourLabel = tk.Label(main, text="ชั่วโมง", bg="#a6edff", fg="Black", font=10).place(x=50, y=win_height - 120)
-minuteLabel = tk.Label(main, text="นาที", bg="#a6edff", fg="Black",font=10).place(x=140, y=win_height - 120)
+hourLabel = tk.Label(main, text="ชั่วโมง", bg="#a6edff", fg="Black", font=10).place(x=60, y=win_height - 120)
+minuteLabel = tk.Label(main, text="นาที", bg="#a6edff", fg="Black",font=10).place(x=150, y=win_height - 120)
 
 #Button
-shutdownTimerButton = tk.Button(main, text="ตั้งเวลาปิดเครื่อง",bg="#96ffb6").place(x=10, y=win_height - 60)
-abortShutdownButton  = tk.Button(main, text="ยกเลิกปิดเครื่อง", bg="#ff8593").place(x=190, y=win_height - 60, anchor="ne")
+shutdownTimerButton = tk.Button(main, text="ตั้งเวลาปิดเครื่อง",bg="#96ffb6", command=lambda: setShutdownTimer(int(hours.get()), int(minutes.get()))).place(x=10, y=win_height - 60)
+abortShutdownButton  = tk.Button(main, text="ยกเลิกปิดเครื่อง", bg="#ff8593", command=abortShutdownTimer).place(x=190, y=win_height - 60, anchor="ne")
 
 main.title("Shutdown Timer")
 main.geometry(f"{win_width}x{win_height}+{x}+{y}")
